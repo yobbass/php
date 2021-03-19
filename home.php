c5f694b46d92b6e6720d5f5d6db721028747f6fc
smon14300 <79419547+smon14300@users.noreply.github.com>
2021-03-19 19:16:55 +0100

    update

---
+++ home.php
@@ -0,0 +1,4 @@
+<?php
+$usr = $_REQUEST['username'];
+echo $usr;
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+?>
+
+<form action="home.php" method="post">
+	<input type="text"  name="username" placeholder="username"><br>
+	<input type="password" name="password" placeholder="password"><br>
+	<input type="submit" value="submit">
+</form>
